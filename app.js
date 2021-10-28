692af5dbb79be28c828e21b53a4e3bd13a0bd518
Luz Sanchez <nena_baby23@hotmail.com>
2021-10-27 19:01:06 -0600

    updates

---
+++ app.js
@@ -0,0 +1,11 @@
+console.log('Pizza Assignment');
+
+var firstName= "Analy";
+var pizzaCount= 4;
+var totalCost= true;
+var IsHungry= "pizzaCount" * 10.00;
+
+alert ("Welcome to Analy's Pizza Restaurant");
+alert ("Our current special is buy 4 pizzas and get one free");
+confirm ("Sounds like your little one is hungry?");
+prompt ("The total cost is $20.00 for the 4 pizzas and you get one free. Would you take this offer?");
